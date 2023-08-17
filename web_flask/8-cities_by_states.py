fed549240d045d80417d6881d9dcd8f54e152a7d
Alvarez0Jose <josealejandro220@gmail.com>
2023-08-17 11:16:55 -0400

    adding tasks 7-9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''States File'''
+
+from flask import Flask, render_template
+from models import *
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Return list of states in Database """
+    state_li = storage.all(State).values()
+    return render_template('7-states_list.html', states=state_li)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    ''' Returns list of City objects '''
+    state_li = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=state_li)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(exception):
+    """ Close Session """
+    storage.close()
+
+    if __name__ == '__main__':
+        app.run(host='0.0.0.0')
